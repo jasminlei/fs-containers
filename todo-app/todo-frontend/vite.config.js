b746b7757db7aae07f5f72a3df179a1c278eb667
jasminlei <jasminleinonen@hotmail.com>
2026-09-12 19:12:37 +0300

    exercise 17

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    allowedHosts: ['frontend'],
+  },
+
   test: {
     environment: 'jsdom',
     globals: true,
